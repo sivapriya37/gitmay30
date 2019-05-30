92cf57193a38774803b96e1b4dca391149fc6965
unknown <sivapriya3793@gmail.com>
2019-05-30 12:00:38 +0530

    changes by user1

--- src/siva/company.java
+++ src/siva/company.java
@@ -7,11 +7,16 @@ public class company {
 	}
      public void companyname() {
     	 System.out.println("companyname is techm");
+    	 
+    	
+     }
+     public void companybus() {
+    	 System.out.println("company bus is 24");
      }
      public static void main(String[] args) {
 		company c =new company();
 		c.companyid();
-
+c.companybus();
 		c.companyname();
 		emp e =new emp();
 		e.employeename();
