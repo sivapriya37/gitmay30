89a75660e4a0dc0437485d0ba88f9bea27f2900e
unknown <sivapriya3793@gmail.com>
2019-05-30 11:42:59 +0530

    second one to git

---
+++ src/siva/company.java
@@ -0,0 +1,25 @@
+package siva;
+
+public class company {
+	
+	public void companyid() {
+		System.out.println("companyid is 123");
+	}
+     public void companyname() {
+    	 System.out.println("companyname is techm");
+     }
+     public static void main(String[] args) {
+		company c =new company();
+		c.companyid();
+
+		c.companyname();
+		emp e =new emp();
+		e.employeename();
+		
+		
+		
+		
+		
+	}
+}
+
---
+++ src/siva/emp.java
@@ -0,0 +1,13 @@
+package siva;
+
+public class emp {
+	public void employeename() {
+	
+	System.out.println("employee name is pooja");
+	
+	
+	}
+	
+	
+
+}
